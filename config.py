01e14e262afb8a611fe486bc0488b57a866beb5f
Rizz <123063244+white-wolf-mia@users.noreply.github.com>
2024-09-03 00:12:50 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,12 @@
+# devggn
+# Note if you are trying to deploy on vps then directly fill values in ("")
+
+from os import getenv
+
+API_ID = int(getenv("API_ID", "21857983"))
+API_HASH = getenv("API_HASH", "e469e84c943ce3b8b056eb6a296f2c67")
+BOT_TOKEN = getenv("BOT_TOKEN", "7306696666:AAHwvcJ8RcIljswKIropp4H2iEHUkvV3lTU")
+OWNER_ID = list(map(int, getenv("OWNER_ID", "833465134").split()))
+MONGO_DB = getenv("MONGO_DB", "mongodb+srv://dhimanrajat:Y8IAGI0lVrMhjvkU@cluster0.mytkgu6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+LOG_GROUP = getenv("LOG_GROUP", "2164681451")
+CHANNEL_ID = int(getenv("CHANNEL_ID", "rixxwillfix"))
