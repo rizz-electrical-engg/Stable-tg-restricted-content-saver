fa3b559196265b6a0024d90215da3d897005613f
Rizz <123063244+white-wolf-mia@users.noreply.github.com>
2024-09-03 09:15:15 +0530

    Update func.py

--- devgagan/core/func.py
+++ devgagan/core/func.py
@@ -39,7 +39,7 @@ async def subscribe(app, message):
             await message.reply_text("Sorry Sir, You are Banned. Contact -- @RD_C4")
             return 1
       except UserNotParticipant:
-         await message.reply_photo(photo="https://graph.org/Rizz-Education-08-30.jpg",caption=script.FORCE_MSG.format(message.from_user.mention), reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Join Now...", url=f"https://t.me/rixxwillfix")]]))
+         await message.reply_photo(photo="https://graph.org/file/6963b6535703e8d7f971a.jpg",caption=script.FORCE_MSG.format(message.from_user.mention), reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Join Now...", url=f"https://t.me/rixxwillfix")]]))
          return 1
       except Exception:
          await message.reply_text("Something Went Wrong. Contact us @RD_C4...")
