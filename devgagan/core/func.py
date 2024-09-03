74b627c67cfd12341b8eafb1bb2c9e7e3525a674
Rizz <123063244+white-wolf-mia@users.noreply.github.com>
2024-09-03 06:52:51 +0530

    Update func.py

--- devgagan/core/func.py
+++ devgagan/core/func.py
@@ -36,7 +36,7 @@ async def subscribe(app, message):
       try:
          user = await app.get_chat_member(update_channel, message.from_user.id)
          if user.status == "kicked":
-            await message.reply_text("Sorry Sir, You are Banned. Contact -- @devggn")
+            await message.reply_text("Sorry Sir, You are Banned. Contact -- @RD_C4")
             return 1
       except UserNotParticipant:
          await message.reply_photo(photo="https://graph.org/Rizz-Education-08-30.jpg",caption=script.FORCE_MSG.format(message.from_user.mention), reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Join Now...", url=f"https://t.me/rixxwillfix")]]))
