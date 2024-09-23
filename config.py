59bc2ca0f83526ac31f2449c8a14fd1eb3309568
Rizz <123063244+rizz-electrical-engg@users.noreply.github.com>
2024-09-23 16:32:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,4 +9,4 @@ BOT_TOKEN = getenv("BOT_TOKEN", "7921569049:AAHBcRXzOpIi-oK68RnfJ9-3SA-yUDKrvG0"
 OWNER_ID = list(map(int, getenv("OWNER_ID", "833465134").split()))
 MONGO_DB = getenv("MONGO_DB", "mongodb+srv://dhimanrajat:Y8IAGI0lVrMhjvkU@cluster0.mytkgu6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 LOG_GROUP = getenv("LOG_GROUP", "-1002164681451")
-CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002482084911"))
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002221310765"))
